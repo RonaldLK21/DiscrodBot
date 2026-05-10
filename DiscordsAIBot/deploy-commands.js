ac4baa0dcfa9bb8c8fba09d2d269006b3e599ba3
RonaldLK21 <rony.lakatso15@outlook.com>
2026-05-11 09:36:46 +1200

    added /remindme it remind the user

--- DiscordsAIBot/deploy-commands.js
+++ DiscordsAIBot/deploy-commands.js
@@ -94,6 +94,21 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN);
 
+try {
+    console.log('Registering slash commands...');
+
+    await rest.put(
+        Routes.applicationGuildCommands(
+            process.env.CLIENT_ID,
+            process.env.GUILD_ID
+        ),
+        { body: commands }
+    );
+
+    console.log('Slash commands registered!');
+} catch (error) {
+    console.error(error);
+}
 
 
 try {
@@ -101,12 +116,12 @@ try {
         
     await rest.put(
        Routes.applicationCommands(
-            process.env.CLIENT_ID
+            process.env.CLIENT_ID,
         ),
         { body: commands }
     );
 
     console.log('Slash commands registered!');
 } catch (error) {
-   // console.error(error);
+   console.error(error);
 }
